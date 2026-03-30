017da428158871c28347548bf851db1221c58e98
geethaimmanuvel <igeethaimmanuvel1_mca27@mepcoeng.ac.in>
2026-03-30 22:02:12 +0530

    Create TagContentExtractor.java

---
+++ TagContentExtractor.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class 	TagContentExtractor
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String line = in.nextLine();
+			
+          	//Write your code here
+            String regex="<([^>]+)>([^<]+)<\\/\\1>";
+            Pattern p=Pattern.compile(regex);
+            Matcher m=p.matcher(line);
+            boolean b=false;
+            while(m.find())
+            {
+                System.out.println(m.group(2));
+                b=true;
+            }
+            if(!b)
+            {
+                System.out.println("None");
+            }
+            
+			
+			testCases--;
+		}
+        in.close();
+	}
+}
+
+
+
